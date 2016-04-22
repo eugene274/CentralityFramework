537100397040ee7ede4e04b5433381c2f455028a
Viktor Klochkov <klochkov44@gmail.com>
2016-04-22 17:22:10 +0200

    Update

--- main.C
+++ main.C
@@ -3,10 +3,8 @@
 #include "NA61DataEvent.h"
 #include "CentralityManager.h"
 
-int main()
+int main(Int_t RunId = 23006)
 {
-
-    Int_t RunId = 23006;
    
     TString na61datadir = "/lustre/nyx/cbm/users/vblinov/NA61/QAtree/QA_15_12_09/";
 //     TString InFileName = "";
@@ -20,46 +18,42 @@ int main()
     
     CentralityManager *manager = new CentralityManager;
     manager->SetDirectory(dir);
-    
-    manager->AddDetector("PSD1");
-    manager->AddDetector("PSD2");
-    manager->AddDetector("PSD3");
-    manager->AddDetector("TPC");
-
     manager->SetRunId (RunId);
-    manager->SetContainerFileName ( ContainerFile );  
+    
 
-//     manager->CopyNa61ExpDataToContainer(na61datadir);
-    manager->IsSimData(false);
-    manager->Det1IsInt(true);
-    manager->Do1DAnalisys(false);
-    manager->SetDetectorsForCentralityAnalisys ("TPC", "PSD1");
-    manager->SetCentralityMax(60);
-    manager->SetDirectionCentralEvents(1);
-    manager->SetSliceStep (5);
-    manager->SetCuts (cuts);
-    
-    manager->RunSliceFinder(RunId);
+//     
+//  For CentralityFinder 
+//  ************************************   
 
-    manager->WriteCentralityFile();
-    
-    
-//     manager->SetDetectorsForCentralityAnalisys ("PSD1", "PSD2");
+//     manager->AddDetector("PSD1");
+//     manager->AddDetector("PSD2");
+//     manager->AddDetector("PSD3");
+//     manager->AddDetector("TPC");  
+//     manager->SetContainerFileName ( ContainerFile );  
+// //     manager->CopyNa61ExpDataToContainer(na61datadir);
+//     manager->IsSimData(false);
+//     manager->Det1IsInt(true);
 //     manager->Do1DAnalisys(false);
-//     manager->SetDirectionCentralEvents(0);
-//     manager->RunSliceFinder();
+//     manager->SetDetectorsForCentralityAnalisys ("TPC", "PSD1");
+//     manager->SetCentralityMax(60);
+//     manager->SetDirectionCentralEvents(1);
+//     manager->SetSliceStep (5);
+//     manager->SetCuts (cuts);
+//     
+//     manager->RunSliceFinder(RunId);
 // 
 //     manager->WriteCentralityFile();
-
     
+    
+//  For CentralityGetter
+//  ************************************   
     
     float c = -1;
     manager->LoadCentalityDataFile( dir + "root_files/Slices_TPC_PSD1_23006.root");
     c = manager->GetCentrality (250, 25);
     std::cout << "Centrality = " << c << std::endl;
-// 
-    delete manager;    
-// 
+        
+    
     return 0;     
 }
 
