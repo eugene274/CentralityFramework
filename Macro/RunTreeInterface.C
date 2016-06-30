67493a57867ad73142f943dd962fbe1f9497201c
Viktor Klochkov <klochkov44@gmail.com>
2016-06-30 12:46:39 +0200

    update

--- Macro/RunTreeInterface.C
+++ Macro/RunTreeInterface.C
@@ -1,11 +1,11 @@
 
-int RunTreeInterface (Int_t FileNum=0)
+int RunTreeInterface (Int_t FileNum=1)
 {
     TString CentralityFrameworkDir = "/lustre/nyx/cbm/users/klochkov/soft/CentralityFramework/";    
     gSystem->Load( CentralityFrameworkDir + "build/libCentrality");
 
-    TString OutFileName = CentralityFrameworkDir + "containers/" + Form("root_files/na61_container_%d.root", FileNum);
-    TString InFileName = CentralityFrameworkDir + "input/" + "Merged_short.root";
+    TString OutFileName = CentralityFrameworkDir + "containers/" + Form("cbm_urqmd_CC_%d.root", FileNum);
+    TString InFileName = CentralityFrameworkDir + "input/" + Form("%d.root", FileNum);
     
     TreeInterface *ti = new TreeInterface();
     ti->SetOutFileName (OutFileName) ;
